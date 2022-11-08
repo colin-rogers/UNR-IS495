67b631611581a0d38b93957a8938d87dc21d556d
Colin Rogers <colinrogers2790@gmail.com>
2022-11-08 14:42:23 -0800

    Update createTable.sql

--- Code/SQL-Code/createTable.sql
+++ Code/SQL-Code/createTable.sql
@@ -1,12 +1,30 @@
-CREATE TABLE author (
+CREATE TABLE Author (
 	AuthorID int,
 	FirstName varchar(255),
 	LastName varchar(255)
 );
 
-CREATE TABLE publisher (
+CREATE TABLE Publisher (
 	PublisherID int,
 	FirstName varchar(255),
 	LastName varchar(255),
 	YearPublished varchar(4)
+);
+
+CREATE TABLE Book (
+	RFID int,
+	Title varchar(255),
+	Edition int,
+  	AuthorID int,
+  	GenreID int,
+  	PublisherID int
+);
+
+CREATE TABLE BookRecord (
+  	RecordID int,
+	CheckOutDate datetime,
+	ReturnDate datetime,
+	RFID int,
+	FineID int,
+  	UserID int
 );
