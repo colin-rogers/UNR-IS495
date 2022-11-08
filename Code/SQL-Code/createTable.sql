660e8e6c43234aec07ba3713a52424898133ee65
Colin Rogers <colinrogers2790@gmail.com>
2022-11-08 14:48:55 -0800

    Update createTable.sql

--- Code/SQL-Code/createTable.sql
+++ Code/SQL-Code/createTable.sql
@@ -1,30 +1,38 @@
 CREATE TABLE Author (
-	AuthorID int,
+	AuthorID int PRIMARY KEY,
 	FirstName varchar(255),
 	LastName varchar(255)
 );
 
 CREATE TABLE Publisher (
-	PublisherID int,
+	PublisherID int PRIMARY KEY,
 	FirstName varchar(255),
 	LastName varchar(255),
 	YearPublished varchar(4)
 );
 
 CREATE TABLE Book (
-	RFID int,
+	RFID int PRIMARY KEY,
 	Title varchar(255),
 	Edition int,
   	AuthorID int,
   	GenreID int,
-  	PublisherID int
+  	PublisherID int,
+	
+	FOREIGN KEY (AuthorID) REFERENCES Author(AuthorID),
+	FOREING KEY (GenreID) REFERENCES Genre(GenreID),
+	FOREIGN KEY (PublisherID) REFERENCES Publisher(PublisherID)
 );
 
 CREATE TABLE BookRecord (
-  	RecordID int,
+  	RecordID int PRIMARY KEY,
 	CheckOutDate datetime,
 	ReturnDate datetime,
 	RFID int,
 	FineID int,
   	UserID int
+	
+	FOREIGN KEY (RFID) REFERENCES Book(RFID),
+	FOREIGN KEY (FineID) REFERENCES Fine(FineID),
+	FOREIGN KEY (UserID) REFERENCES User(UserID)
 );
